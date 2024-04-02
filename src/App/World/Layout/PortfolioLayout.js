c70db8e80bd19aa5385f8f307c72b8c4ea0a5884
LeeBingler <binglerlee@gmail.com>
2024-04-02 16:12:55 +0200

    feat: add logic for portfolio popup

--- src/App/World/Layout/PortfolioLayout.js
+++ src/App/World/Layout/PortfolioLayout.js
@@ -5,10 +5,44 @@ export default class PortfolioLayout {
         this.app = new App();
 
         this._initSection();
+        this._initEvent();
     }
 
     _initSection() {
         this.section = document.querySelector('.portfolio-section');
+        this.title = document.querySelector('.portfolio-section .title');
+        this.backButton = document.querySelector('.back-button');
+        this.holder = document.querySelector('.portfolio-holder');
+        this.portfolio = this.getProject('portfolio');
+    }
+
+    _initEvent() {
+        this.backButton.addEventListener('click', () => {
+            this.holder.style.display = "block";
+            this.title.style.display = "flex";
+            this.backButton.style.display = "none";
+            this.current.popup.style.display = "none";
+            this.current = null;
+        })
+        
+        this.setEvent(this.portfolio);
+    }
+
+    getProject(str) {
+        const project = document.querySelector(`.${str}-project`);
+        const popup = document.querySelector(`.${str}-popup`);
+
+        return {project, popup};
+    }
+
+    setEvent(obj) {
+        obj.project.addEventListener('click', () => {
+            this.holder.style.display = "none";
+            this.title.style.display = "none";
+            this.backButton.style.display = "block";
+            obj.popup.style.display = "block";
+            this.current = obj;
+        })
     }
 
     addActiveOnSection() {
